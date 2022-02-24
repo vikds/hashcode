86afa26d0cddcd963e98df43f9f6e9b886a1ce6f
Dmitry Vikharev <dmitry.vikharev@gmail.com>
2022-02-25 01:09:27 +0300

    Qualification Round

--- 2022/cpp/src/main.cpp
+++ 2022/cpp/src/main.cpp
@@ -8,6 +8,7 @@
 #include <iterator>
 #include <limits>
 #include <iomanip>
+#include <queue>
 #include <random>
 #include <sstream>
 #include <string>
@@ -18,6 +19,7 @@
 #include <vector>
 
 static const std::string VERSION = "2022.02.24 21:33";
+static const int MAX_DAY = 1e5;
 
 void Usage(const char* binary_name) {
     std::cerr << "Usage: " << binary_name << " [-io] [-i input][-o output]" << std::endl;
@@ -107,6 +109,7 @@ using Skills = std::vector<Skill>;
 struct Contrib {
     std::string name;
     Skills skills;
+    bool busy = false;
 };
 
 using Contribs = std::vector<Contrib>;
@@ -117,6 +120,7 @@ struct Project {
     int score;
     int deadline;
     Skills skills;
+    bool done = false;
 };
 
 using Projects = std::vector<Project>;
@@ -166,12 +170,21 @@ void ReadInput(const Arguments& args, Input& data) {
 struct Assign {
     std::string project_name;
     Strings contrib_names;
+    int end_day;
+};
+
+struct AssignCmp {
+    bool operator()(const Assign& lhs, const Assign& rhs) {
+        return lhs.end_day > rhs.end_day; // for min_heap
+    }
 };
 
 using Assigns = std::vector<Assign>;
+using AssignsHeap = std::priority_queue<Assign, Assigns, AssignCmp>;
 
 struct Output {
     Assigns assigns;
+    int score = 0;
 };
 
 void PrintOutput(const Arguments& args, Output& data) {
@@ -179,21 +192,149 @@ void PrintOutput(const Arguments& args, Output& data) {
     output << data.assigns.size() << std::endl;
     for (const Assign& assign: data.assigns) {
         output << assign.project_name << std::endl;
-        bool first = false;
+        bool first = true;
         for (const std::string& contrib_name: assign.contrib_names) {
             if (!first) {
                 output << " ";
             }
             output << contrib_name;
+            first = false;
         }
         output << std::endl;
     }
 }
 
-strcut Model {
-
+enum class Order {
+    Shortest = 0,
+    Largest = 1,
+    Nearest = 2,
+    NUM = 3,
 };
 
+void FreeContribs(const Strings& contrib_names, Contribs& contribs) {
+    for (const std::string& contrib_name: contrib_names) {
+        for (Contrib& contrib: contribs) {
+            if (contrib_name != contrib.name) {
+                continue;
+            }
+            contrib.busy = false;
+            break;
+        }
+    }
+}
+
+void ImproveSkills(const Project& project, const Strings& contrib_names, Contribs& contribs) {
+    for (int i = 0; i < project.skills.size(); i++) {
+        const std::string& contrib_name = contrib_names[i];
+        for (Contrib& contrib: contribs) {
+            if (contrib_name != contrib.name) {
+                continue;
+            }
+
+            bool found = false;
+            const Skill& requirement = project.skills[i];
+            for (Skill& skill: contrib.skills) {
+                if (requirement.name != skill.name) {
+                    continue;
+                }
+
+                found = true;
+                if (requirement.level >= skill.level) {
+                    skill.level++;
+                }
+                break;
+            }
+            if (!found) {
+                Skill skill;
+                skill.level = 1;
+                skill.name = requirement.name;
+                contrib.skills.push_back(skill);
+            }
+            break;
+        }
+    }
+}
+
+Strings AssignContribs(const Project& project, Contribs& contribs) {
+    Strings result;
+    for (const Skill& requirement: project.skills) {
+        bool covered = false;
+        for (Contrib& contrib: contribs) {
+            if (contrib.busy) {
+                continue;
+            }
+
+            for (const Skill& skill: contrib.skills) {
+                if (skill.name != requirement.name) {
+                    continue;
+                }
+                if (skill.level < requirement.level) {
+                    continue;
+                }
+                covered = true;
+                break;
+            }
+            if (!covered) {
+                continue;
+            }
+
+            contrib.busy = true;
+            result.push_back(contrib.name);
+            break;
+        }
+        if (!covered) {
+            break;
+        }
+    }
+    if (result.size() < project.skills.size()) {
+        FreeContribs(result, contribs);
+        result.clear();
+    }
+    return result;
+}
+
+Output ExcetuteInProjectsOrder(Input& model) {
+    Output output;
+
+    int day = 0;
+    int idx = 0;
+    AssignsHeap assigns_heap;
+    while (idx < model.projects.size() || !assigns_heap.empty()) {
+        for (;idx < model.projects.size(); idx++) {
+            Project& project = model.projects[idx];
+            if (project.done) {
+                continue;
+            }
+            Strings contribs = AssignContribs(project, model.contribs);
+            if (contribs.empty()) {
+                continue;
+            }
+
+            Assign assign;
+            assign.project_name = project.name;
+            assign.contrib_names = contribs;
+            assign.end_day = day + project.duration;
+            output.assigns.push_back(assign);
+            assigns_heap.push(assign);
+
+            ImproveSkills(project, contribs, model.contribs);
+            output.score += project.score;
+            project.done = true;
+        }
+        if (!assigns_heap.empty()) {
+            Assign assign = assigns_heap.top();
+            assigns_heap.pop();
+
+            FreeContribs(assign.contrib_names, model.contribs);
+            day = assign.end_day;
+            idx = 0;
+        }
+        if (day > MAX_DAY) {
+            break;
+        }
+    }
+    return output;
+}
 
 
 int main(int argc, char* argv[]) {
@@ -211,8 +352,38 @@ int main(int argc, char* argv[]) {
         Input input;
         ReadInput(args, input);
 
-        Output output;
-        PrintOutput(args, output);
+        Output result;
+        for (int i = 0; i < static_cast<int>(Order::NUM); i++) {
+            Input model = input; // reset
+            Output current;
+
+            Order order = static_cast<Order>(i);
+            switch (order) {
+                case Order::Shortest:
+                    std::sort(model.projects.begin(), model.projects.end(), [&](const Project& lhs, const Project& rhs) {
+                        return lhs.duration < rhs.duration;
+                    });
+                    current = ExcetuteInProjectsOrder(model);
+                    break;
+                case Order::Largest:
+                    std::sort(model.projects.begin(), model.projects.end(), [&](const Project& lhs, const Project& rhs) {
+                        return lhs.score < rhs.score;
+                    });
+                    current = ExcetuteInProjectsOrder(model);
+                    break;
+                case Order::Nearest:
+                    std::sort(model.projects.begin(), model.projects.end(), [&](const Project& lhs, const Project& rhs) {
+                        return lhs.deadline < rhs.deadline;
+                    });
+                    current = ExcetuteInProjectsOrder(model);
+                    break;
+            }
+            if (current.score > result.score) {
+                result = current;
+            }
+        }
+
+        PrintOutput(args, result);
 
     } catch(const std::exception& ex) {
         return EXIT_FAILURE;
